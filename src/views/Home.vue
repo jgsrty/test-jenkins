dcc193e3f74443a8bd7aaddebcab14fbed804e96
荣天阳 <573850993@qq.com>
2019-01-17 09:23:08 +0800

    test-jenkins

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="2019-1-17 test Jenkins"/>
   </div>
 </template>
 
